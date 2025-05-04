d2056cbfc5cfdeaa8bc610340b71c5ecbddaa613
assassin <robinmulder1205@outlook.com>
2025-05-05 01:18:34 +0900

    Updated the TransactionPreview for state of confirm button

--- src/components/screens/transaction/TransactionPreview.tsx
+++ src/components/screens/transaction/TransactionPreview.tsx
@@ -1,11 +1,12 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import { ActionButtons } from '@/components/screen-ui/buttons'
 import { TransactionDetailCard } from '@/components/screen-ui/cards'
 import { LoadingScreen } from '@/components/screen-ui/feedback'
 import { CommonContainerPanel } from '@/components/screen-ui/layout'
 import { BottomNavigation, HeaderNavigation } from '@/components/screen-ui/navigation'
-import { useConfig, useScreenManager } from '@/hooks'
+import { useConfig, useScreenManager, usePaymasterContext } from '@/hooks'
 import { TransactionPreviewProps, screens } from '@/types'
+import { SendContext } from '@/contexts'
 
 const TransactionPreview: React.FC<TransactionPreviewProps> = ({
   from,
@@ -23,6 +24,11 @@ const TransactionPreview: React.FC<TransactionPreviewProps> = ({
   const [loading, setLoading] = useState(false)
   const [completed, setCompleted] = useState(false)
   const [userOpResult, setUserOpResult] = useState(false)
+
+  const { isTransferEnabled } = useContext(SendContext)!
+  const { clearToken, selectedMode, isPaymentSelected } = usePaymasterContext()
+  const isTransferReady =
+    isTransferEnabled && selectedMode?.value !== undefined && isPaymentSelected
 
   // ガス計算中かどうかを判定
   const isCalculatingGas = networkFee === 'Calculating...'
@@ -60,7 +66,7 @@ const TransactionPreview: React.FC<TransactionPreviewProps> = ({
       <HeaderNavigation />
       <div className='mx-auto relative px-6'>
         <div className='flex flex-col flex-grow'>
-          <div className='w-full bg-white rounded-md border border-border-primary items-center justify-center p-3 mt-2'>
+          <div className='w-full h-[530px] bg-white rounded-md border border-border-primary items-center justify-center p-3 mt-2 relative'>
             <h2 className='text-xl text-center text-text-secondary mb-3'>{title}</h2>
             <TransactionDetailCard
               from={from}
@@ -68,12 +74,14 @@ const TransactionPreview: React.FC<TransactionPreviewProps> = ({
               networkFee={networkFee}
               gasTokenSymbol={gasTokenSymbol}
               networkType={networkType}
+              title={title}
             >
               {children}
             </TransactionDetailCard>
             <ActionButtons
-              onBack={onClose}
-              onNext={handleConfirm}
+              onCancel={onClose}
+              onConfirm={handleConfirm}
+              confirmDisabled={!isTransferReady}
               nextLabel={
                 loading ? 'Processing...' : isCalculatingGas ? 'Calculating Gas...' : 'Confirm'
               }
