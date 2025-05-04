2b38f5927837b8e3e33fe916fe2434e81cd68d18
assassin <robinmulder1205@outlook.com>
2025-05-05 01:11:37 +0900

    Added the abi file for interaction with smart contract

---
+++ src/config/myContractAddress.ts
@@ -0,0 +1,7 @@
+// Contract addresses - replace with your own as needed
+export const CONTRACT_ADDRESSES = {
+    // Example NFT contract address - replace with your own
+    AUDIOFI_TOKEN_ADDRESS: "0xDA4EaAdD6B536654f0789264D4882a2b85E31f9d",
+    STAKING_CONTRACT_ADDRESS: "0xEf48D62C20448ED84ee5C65c594CD2DE67F0B157",
+    nftContract: "0x63f1f7c6a24294a874d7c8ea289e4624f84b48cb",
+  };
